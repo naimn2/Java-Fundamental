8bd621d2c3b573874547a964c168eec571c44937
Muhammad Muflihun Naim <muflihunnaim275@gmail.com>
2019-09-08 16:19:04 +0700

    Mengubah nama file Tes.java menjadi FPB.java

--- FPB.java
+++ FPB.java
@@ -1,4 +1,4 @@
-public class Tes {
+public class FPB {
     public static void main(String[] args){
     	String str = "I can show you the world";
         System.out.println("I can show you the world");
