7fcbbd48e6c5f63912bbddb8f0c28101a9762854
Muhammad Muflihun Naim <muflihunnaim275@gmail.com>
2019-09-08 16:30:37 +0700

    Memperbaharui file FPB.java

--- FPB.java
+++ FPB.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class FPB {
     public static void main(String[] args){
-    	String str = "I can show you the world";
-        System.out.println("I can show you the world");
+    	Scanner scan = new Scanner(System.in);
+
+    	int a = scan.nextInt();
+    	int b = scan.nextInt();
+
+    	do {
+    		int mod = a%b;
+    		a = b;
+    		b = mod;
+    	} while (b > 0);
+
+    	System.out.println("FPB nya adalah "+a);
     }
 }
